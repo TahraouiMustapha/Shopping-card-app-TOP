96975d1613041eeb2815e81f888530d5190cb575
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-03-26 18:19:32 +0100

    feat: create a slide component

---
+++ src/components/slide/Slide.jsx
@@ -0,0 +1,47 @@
+import styles from './Slide.module.css'
+import { useState } from 'react'
+
+
+function Card({number}) {
+    return (
+        <div> {number} </div>
+    )
+}
+
+const arrayOfObjs = [
+  {number:0},
+  {number:1},
+  {number:2},
+  {number:3},
+  {number:4},
+  {number:5}
+]
+
+export default function Slide() {
+    const [shownIndex, setShownIndex] = useState([
+        0, 1, 2, 3
+    ])
+
+
+    function handleLiftClick() {
+        setShownIndex((prevShownIndex) => 
+            prevShownIndex.map((shown)=> shown <= 0? arrayOfObjs.length - 1: shown -= 1 )
+        )
+    }
+
+    function handleRightClick() {
+        setShownIndex((prevShownIndex)=> 
+            prevShownIndex.map((shown)=> shown >= arrayOfObjs.length - 1? 0: shown += 1)
+        )
+    }
+
+    return (
+        <div className={styles.slide}>
+            <button onClick={handleLiftClick}>&lt;</button>
+            <div className={styles.cardsContainer}>
+                {shownIndex.map((shown)=> <Card number={arrayOfObjs[shown].number}/>)}
+            </div>
+            <button onClick={handleRightClick}>&gt;</button>
+        </div>
+    )
+}
