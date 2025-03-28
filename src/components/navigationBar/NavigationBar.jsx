54fb1a9745279256f971d3fa6935e5b4449b762e
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-03-28 06:41:30 +0100

    feat: complete the navigation bar component with its style

--- src/components/navigationBar/NavigationBar.jsx
+++ src/components/navigationBar/NavigationBar.jsx
@@ -1,11 +1,37 @@
 import { Link } from "react-router-dom"
+import styles from "./NavigationBar.module.css"
+import { Search, ShoppingCart } from "lucide-react"
+
+
+
+function NavLink({title, children}) {
+    return(
+        <div className={styles.navLink}>
+            {children}
+            <p>{title}</p>
+        </div>
+    )
+}
 
 function NavigationBar() {
     return (
-        <>
-            <Link to="/"> Home </Link>
-            <Link to="shop"> Shop </Link>
-        </>
+        <div className={styles.navigationBar}>
+
+            <div>
+                <h1 className={styles.logo}>MYLib</h1>
+                <Link to="/"> Home </Link>
+                <Link to="shop"> Shop </Link>
+            </div>
+
+            <div>
+                <NavLink title={'Search'}>
+                    <Search size={32}/>
+                </NavLink>
+                <NavLink title={'Cart'}>
+                    <ShoppingCart size={32}/>
+                </NavLink>
+            </div>
+        </div>
     )
 }
 
