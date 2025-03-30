dcc06cc774759f9b8be462f23975db3af1f4e5c6
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-03-30 23:16:25 +0100

    fix: handle the absence of book's informations

--- src/components/bookCard/Card.jsx
+++ src/components/bookCard/Card.jsx
@@ -2,16 +2,26 @@ import styles from "./Card.module.css"
 
 
 export default function Card({book}) {
+    const thumbnailStyle = {
+        backgroundImage: book?.thumbnail ? `url(${book.thumbnail})`: 'none',
+        backgroundColor: '#f3f3f3',
+        textAlign: 'center',
+        color: '#666'
+    }
+
+
     return (
         <div className={styles.card}>
             <div 
             className={styles.thumbnail}
-            style={{backgroundImage: `url(${book.thumbnail})`}}></div>
+            style={thumbnailStyle}>
+                {!book?.thumbnail && 'No image found'}
+            </div>
 
             <div className={styles.content}>
                 <div>
-                    <p className={styles.title}>{book.title}</p>
-                    <p className={styles.author}>{book.author}</p>
+                    <p className={styles.title}>{book?.title}</p>
+                    <p className={styles.author}>{book?.author}</p>
                 </div>
                 <div>
                     <p className={styles.price}>12$</p>
