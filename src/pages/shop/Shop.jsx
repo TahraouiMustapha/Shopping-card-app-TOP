11477c601e10136e3754c642dd5ec6c7bf23e101
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-03-30 22:30:58 +0100

    feat: write the search component with style the general structure

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,14 +1,40 @@
 import { useOutletContext } from "react-router-dom";
+import styles from "./Shop.module.css";
 
-function Shop() {
-    const { categorie } = useOutletContext()
+import { Search } from 'lucide-react';
 
+
+
+function SearchBar() {
     return (
-        <>
-            <div> i am a shop with {categorie} </div>
-        </>
-
+        <div className={styles.search}>
+            <Search color="#9ca3af"/>
+            <input placeholder="Search for books..."/>
+        </div>
     )
 }
 
-export default Shop;
+function ShopMain() {
+    return (
+        <div className={styles.main}>main</div>
+    )
+}
+
+function ShopSide() {
+    return (
+        <div className={styles.side}>side</div>
+    )
+}
+
+export default function Shop() {
+    const { categorie } = useOutletContext()
+
+    return (
+        <div className={styles.content}>
+            <SearchBar/>
+            <ShopMain/>
+            <ShopSide/>
+        </div>
+
+    )
+};
